8a77f67eb43fc1824ba3d77969014405a87402d1
cheatthearatheara-lab <cheatthearatheara@gmail.com>
2026-09-14 20:40:41 +0700

    Fix CORS for Vercel frontend

---
+++ Mini-mart-back-end/cors.php
@@ -0,0 +1,21 @@
+<?php
+
+$allowedOrigins = [
+    'https://minimart-project-seven.vercel.app',
+];
+
+$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+
+if (in_array($origin, $allowedOrigins, true)) {
+    header("Access-Control-Allow-Origin: $origin");
+    header("Vary: Origin");
+}
+
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Credentials: true");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit;
+}
